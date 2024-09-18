7202221c398bb46e0e27def3bda2e31495f4a1a4
Jorge Martin <jorge.martin.lapena@alumnos.upm.es>
2024-09-18 11:33:25 +0200

    Completed AcmeBankAccounts from 0-bankaccount

--- 0-bankaccount/src/main/java/bankaccount/AcmeBankAccounts.java
+++ 0-bankaccount/src/main/java/bankaccount/AcmeBankAccounts.java
@@ -2,53 +2,69 @@ package bankaccount;
 
 
 
-public class AcmeBankAccounts { 
+public class AcmeBankAccounts {
 
-    int balance = 0;  
-    String holder;  
-    int accountNumber;  
-    int idAccounts = 1; //First number account  to use 
-    int BankBalance = 0; // Total bank balance 
-    AcmeBankAccounts (String holder, int initialBalance) { 
-    // Create a new bank account for a client holder with an initial balance >= 0 
-    // it will assign to the bank account an idAccount 
-    // To be coded by the student 
-    } 
-    
-    void printBankData() { 
-    // Print the total Balance of the bank and the current accounts. 
-    // To be coded by the student 
-    } 
-    
-    void  withdrawal (int amount) { 
-    // For the current account, withdrawal the indicated amount 
-    // Decrement also the Bank balance 
-    // To be coded by the student 
-    
-    } 
-    
-    void deposit(int amount) { 
-    // To be coded by the student 
-    } 
-    
-    void transfer (AcmeBankAccounts destinationAccount, int amount) { 
-    //Transfert the amount to the destination Account 
-    // To be coded by the student 
-    } 
-    
-    void printAccountData() { 
-    // print the account number, holder and  account balance 
-    // To be coded by the student 
-    } 
-    
-    public static void main(String[] args) { 
-    AcmeBankAccounts cJuan = new AcmeBankAccounts ("Juan", 2000); 
-    AcmeBankAccounts cPepe = new AcmeBankAccounts ("Pepe", 3000); 
-    cJuan.printAccountData(); 
-    cPepe.printAccountData(); 
-    cJuan.transfer(cPepe, 1000); 
-    cJuan.printAccountData(); 
-    cPepe.printAccountData(); 
-    //AcmeBankAccounts.printBankData();  
-    } 
+    private int balance = 0;
+    private final String holder;
+    private final int accountNumber;
+    static int idAccounts = 1; //First number account  to use
+    static int bankBalance = 0; // Total bank balance
+    public AcmeBankAccounts (String holder, int initialBalance) {
+        // Create a new bank account for a client holder with an initial balance >= 0
+        // it will assign to the bank account an idAccount
+        // To be coded by the student
+        this.holder = holder;
+        this.balance += initialBalance;
+        AcmeBankAccounts.bankBalance += initialBalance;
+        this.accountNumber = AcmeBankAccounts.idAccounts++;
+    }
+
+    static void printBankData() {
+        // Print the total Balance of the bank and the current accounts.
+        // To be coded by the student
+        System.out.println("-BANK DATA-");
+        System.out.println("\tBankBalance: " + AcmeBankAccounts.bankBalance);
+        System.out.println("\tAccounts: " + (AcmeBankAccounts.idAccounts-1));
+    }
+
+    private void withdrawal (int amount) {
+        // For the current account, withdrawal the indicated amount
+        // Decrement also the Bank balance
+        // To be coded by the student
+        this.balance -= amount;
+        AcmeBankAccounts.bankBalance -= amount;
+    }
+
+    public void deposit(int amount) {
+        // To be coded by the student
+        this.balance += amount;
+        AcmeBankAccounts.bankBalance += amount;
+    }
+
+    public void transfer (AcmeBankAccounts destinationAccount, int amount) {
+        //Transfert the amount to the destination Account
+        // To be coded by the student
+        withdrawal(amount);
+        destinationAccount.deposit(amount);
+    }
+
+    public void printAccountData() {
+        // print the account number, holder and  account balance
+        // To be coded by the student
+        System.out.println("-ACCOUNT DATA-");
+        System.out.println("\taccountNumber: " + accountNumber);
+        System.out.println("\tholder: " + holder);
+        System.out.println("\tbalance: " + balance);
+    }
+
+    public static void main(String[] args) {
+        AcmeBankAccounts cJuan = new AcmeBankAccounts ("Juan", 2000);
+        AcmeBankAccounts cPepe = new AcmeBankAccounts ("Pepe", 3000);
+        cJuan.printAccountData();
+        cPepe.printAccountData();
+        cJuan.transfer(cPepe, 1000);
+        cJuan.printAccountData();
+        cPepe.printAccountData();
+        AcmeBankAccounts.printBankData();
+    }
 } 
