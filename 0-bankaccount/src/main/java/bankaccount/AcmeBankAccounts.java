fbb9fe5d6210f2898acc56fe8779464878f15430
Jorge Martin <jorge.martin.lapena@alumnos.upm.es>
2024-09-25 17:24:15 +0200

    Added Comments from 0-bankaccount

--- 0-bankaccount/src/main/java/bankaccount/AcmeBankAccounts.java
+++ 0-bankaccount/src/main/java/bankaccount/AcmeBankAccounts.java
@@ -1,14 +1,17 @@
 package bankaccount;
 
 
-
 public class AcmeBankAccounts {
 
+    // Atributos privados ya que pertenecen a cada objeto (cliente) que se cree.
     private int balance = 0;
+    // Atributos finales ya que no cambian a lo largo de la ejecución
     private final String holder;
     private final int accountNumber;
+    // Atributos estáticos ya que se deben mantener en todos los objetos AcmeBankAccounts que se creen
     static int idAccounts = 1; //First number account  to use
     static int bankBalance = 0; // Total bank balance
+
     public AcmeBankAccounts (String holder, int initialBalance) {
         // Create a new bank account for a client holder with an initial balance >= 0
         // it will assign to the bank account an idAccount
@@ -19,6 +22,7 @@ public class AcmeBankAccounts {
         this.accountNumber = AcmeBankAccounts.idAccounts++;
     }
 
+    // Estático ya que no importan los clientes que se hayan credo, el metodo ha de poder ejecutarse
     static void printBankData() {
         // Print the total Balance of the bank and the current accounts.
         // To be coded by the student
@@ -27,6 +31,7 @@ public class AcmeBankAccounts {
         System.out.println("\tAccounts: " + (AcmeBankAccounts.idAccounts-1));
     }
 
+    // Privado ya que sólo se accede al metodo desde dentro de la propia clase
     private void withdrawal (int amount) {
         // For the current account, withdrawal the indicated amount
         // Decrement also the Bank balance
@@ -35,6 +40,7 @@ public class AcmeBankAccounts {
         AcmeBankAccounts.bankBalance -= amount;
     }
 
+    // Público ya que se debe de utilizar para realizar las operaciones con la cuenta
     public void deposit(int amount) {
         // To be coded by the student
         this.balance += amount;
